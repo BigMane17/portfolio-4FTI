7e221c46a9da42ff34098d0c8b43bed59bb05e9d
Studente <samu.manenti@gmail.com>
2023-03-24 11:49:59 +0100

    ciao

---
+++ src/components/SectionFlex.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Item from "./Item";
+import { ItemData } from "../data";
+
+const SectionFlex = ({data}) => {
+    const articleData = ItemData.filter(item => item.tag === 'article').slice(0,5);
+
+    console.log(articleData);
+
+    return(
+        <section className = 'section'>
+            {articleData.map (item => <Item key={item.id} data={item} />)}
+     </section>
+    );
+}
+
+export default SectionFlex;
